a64e8036911fa3acc3cf7bff3c9a07e85ef885bf
JangHoon <jqdjhy@naver.com>
2022-03-02 17:18:11 +0900

    to jiyeong

--- server/server.js
+++ server/server.js
@@ -291,7 +291,3 @@ app.delete("/api/TR/delete/:id", loginCheck, function (req, res) {
     return res.send(false);
   });
 });
-
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "../zwontr/build/index.html"));
-// });
